d31d8a6522a6e03b0f74a0dde8a87f51da16fc8c
k.tkd0222 <k.tkd0222@gmail.com>
2021-05-17 22:00:57 +0900

    バックエンドとフロントエンドにDockerファイルを分割 画面の実装

--- app/my-vue-app/src/components/SearchForm.vue
+++ app/my-vue-app/src/components/SearchForm.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="hero is-medium">
     <div class="hero-body has-text-centered">
-      <h1 class="title is-2">Thoughts, stories, and ideas.</h1>
+      <h1 class="title is-2">{{ message }}</h1>
       <div id="hero-input-group" class="field has-addons has-addons-centered">
         <div class="control">
           <input class="input is-medium" type="text" placeholder="Search..." />
@@ -16,6 +16,29 @@
   </section>
 </template>
 
+<script>
+import { axios } from '@/plugins/axios/index'
+
+export default {
+    components: {
+    },
+    data() {
+      return {
+        message : ''
+      }
+    },
+    mounted() {
+      this.get_hello().then((response) =>{
+        this.message = response.data.message
+      })
+    },
+    methods: {
+      get_hello() {
+        return axios.get('api/hello')
+      }
+    },
+}
+</script>
 <style lang="scss" scoped>
 
 </style>
